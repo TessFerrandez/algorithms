4f7060c16fe9312a5effb143e57fb7eb75f8010b
TessFerrandez <tferrand@microsoft.com>
2022-03-20 22:38:43 +0100

    more solutions

---
+++ contest/lc-e-2210-count-hills-and-valleys-in-an-array.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def countHillValley(self, nums: List[int]) -> int:
+        nums = [nums[i] for i in range(len(nums)) if i == 0 or i > 0 and nums[i] != nums[i - 1]]
+        return sum(1 for i in range(1, len(nums) - 1) if (nums[i] > nums[i - 1] and nums[i] > nums[i + 1]) or (nums[i] < nums[i - 1] and nums[i] < nums[i + 1]))
+
+
+solution = Solution()
+assert solution.countHillValley([2, 4, 1, 1, 6, 5]) == 3
+assert solution.countHillValley([6, 6, 5, 5, 4, 1]) == 0
